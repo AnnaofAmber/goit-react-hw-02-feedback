d0855505cedf33bb4b82c34676f412b47498f394
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-19 18:34:19 +0300

    adding basic markup
    component Statistics + method for total feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+
+import { Statistics } from "./Statiscs/Statistic";
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  countTotalFeedback = () =>{
+const {good, neutral,bad} = this.state
+const totalFeedback = good+neutral+bad
+return totalFeedback
+  }
+
+  render(){
+    return(
+      <div>
+        <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.state.good}></Statistics>
+      </div>
+    )
+  }
 };
---
+++ src/components/Statiscs/Statistic.jsx
@@ -0,0 +1,20 @@
+export const Statistics = ({good, neutral, bad, total, positivePercentage}) =>{
+return(
+    <div>
+    <section>
+    <h2>Please leave feedback </h2>
+    <button>Good</button>
+    <button>Neutral</button>
+    <button>Bad</button>
+    </section>
+    <section>
+        <h2>Statistics</h2>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>Total: {total}</p>
+        <p>Positive feedback: {positivePercentage}</p>
+    </section>
+    </div>
+)
+}
